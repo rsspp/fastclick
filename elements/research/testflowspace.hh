a810aba10545a03df148c0e737717b83ada60d9e
Tom Barbette <barbette@kth.se>
2019-10-14 15:04:25 +0200

    Import more flow things from MiddleClick

---
+++ elements/research/testflowspace.hh
@@ -0,0 +1,43 @@
+#ifndef CLICK_TestFlowSpace_HH
+#define CLICK_TestFlowSpace_HH
+#include <click/config.h>
+#include <click/multithread.hh>
+#include <click/hashtablemp.hh>
+#include <click/glue.hh>
+#include <click/vector.hh>
+#include <click/flow/flowelement.hh>
+#include <random>
+
+CLICK_DECLS
+
+struct FourBytes {
+    FourBytes() : w(0) {
+
+	}
+	uint32_t w;
+};
+
+class TestFlowSpace : public FlowSpaceElement<FourBytes> {
+
+public:
+
+    TestFlowSpace() CLICK_COLD;
+    ~TestFlowSpace() CLICK_COLD;
+
+    const char *class_name() const		{ return "TestFlowSpace"; }
+    const char *port_count() const		{ return "1/1"; }
+    const char *processing() const		{ return PUSH; }
+
+    int configure(Vector<String> &, ErrorHandler *) override CLICK_COLD;
+    int initialize(ErrorHandler *errh) override CLICK_COLD;
+
+    void push_batch(int, FourBytes*, PacketBatch *);
+
+};
+
+
+
+
+
+CLICK_ENDDECLS
+#endif
